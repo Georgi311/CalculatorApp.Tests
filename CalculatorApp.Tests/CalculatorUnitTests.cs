5af784e901f9a79f2ef88af3c3fb5c409d5cbd41
stefancho <stefanstoqnov@gmail.com>
2026-01-12 08:21:55 +0200

    Add project files.

---
+++ CalculatorApp.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp;
+
+namespace CalculatorApp.Tests
+{
+    [TestClass]
+    public class CalculatorUnitTest
+    {
+        [TestMethod]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            int result = calc.Add(5, 3);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void Subtract_ShouldReturnCorrectDifference()
+        {
+            var calc = new Calculator();
+            int result = calc.Subtract(10, 4);
+            Assert.AreEqual(6, result);
+        }
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CalculatorApp
+{
+    public class Program
+    {
+        public class Calculator
+        {
+            public int Add(int a, int b) => a + b;
+            public int Subtract(int a, int b) => a - b;
+        }
+
+        public static void Main(string[] args)
+        {
+            var calc = new Calculator();
+
+            Console.WriteLine("=== Simple Calculator ===");
+            Console.Write("Enter first number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{a} + {b} = {calc.Add(a, b)}");
+            Console.WriteLine($"{a} - {b} = {calc.Subtract(a, b)}");
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
